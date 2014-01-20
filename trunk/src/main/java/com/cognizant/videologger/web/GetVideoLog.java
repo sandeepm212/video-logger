e7961111fd8d5dbb81de02ec1959f9cd5a157c80
sandeep.mandrumaka@gmail.com <sandeep.mandrumaka@gmail.com@1f31e873-1701-5331-db18-291316ffd775>
2014-01-20 02:41:00 +0000

    publish video

--- trunk/src/main/java/com/cognizant/videologger/web/GetVideoLog.java
+++ trunk/src/main/java/com/cognizant/videologger/web/GetVideoLog.java
@@ -1,7 +1,10 @@
 package com.cognizant.videologger.web;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
@@ -12,6 +15,9 @@ import javax.servlet.http.HttpServletResponse;
 import com.cognizant.videologger.util.Utils;
 
 public class GetVideoLog extends HttpServlet {
+
+	private static final long serialVersionUID = 2387206841559992696L;
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		doPost(req, resp);
@@ -19,12 +25,59 @@ public class GetVideoLog extends HttpServlet {
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		String data = Utils.readFromFile(new File("videolog.data"));
-		System.out.println("Data saved:: " + data);
-		resp.setContentType("text/json");
-		PrintWriter out = resp.getWriter();
-		
-		out.println(data);
+		if ("true".equals(req.getParameter("publish"))) {
+			downloadLogs(req, resp);
+		} else {
+			String data = Utils.readFromFile(new File("videolog.data"));
+			System.out.println("Data saved:: " + data);
+			resp.setContentType("text/json");
+			PrintWriter out = resp.getWriter();
+			out.println(data);
+		}
 
+	}
+
+	protected void downloadLogs(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		String pid = req.getParameter("pid");
+		if ("y".equals(req.getParameter("show"))) {
+			resp.setContentType("application/json");
+			resp.setHeader("Content-Disposition", "attachment; filename=VideoLogs.json");
+			String data = (String)req.getSession().getAttribute(pid);
+			resp.getOutputStream().print(data);
+			req.getSession().removeAttribute(pid);
+		} else {
+			String jsonData = getJsonBody(req);
+			req.getSession().setAttribute(pid, jsonData);
+		}
+		
+	}
+
+	public String getJsonBody(HttpServletRequest req) throws IOException {
+		StringBuilder jsonBody = new StringBuilder();
+		BufferedReader bufferedReader = null;
+		try {
+			InputStream inputStream = req.getInputStream();
+			if (inputStream != null) {
+				bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+				char[] charBuffer = new char[128];
+				int bytesRead = -1;
+				while ((bytesRead = bufferedReader.read(charBuffer)) > 0) {
+					jsonBody.append(charBuffer, 0, bytesRead);
+				}
+			} else {
+				jsonBody.append("");
+			}
+		} catch (IOException ex) {
+			throw ex;
+		} finally {
+			if (bufferedReader != null) {
+				try {
+					bufferedReader.close();
+				} catch (IOException ex) {
+					throw ex;
+				}
+			}
+		}
+		return jsonBody.toString();
 	}
 }
--- trunk/src/main/webapp/video-poc/js/controller/step3Controller.js
+++ trunk/src/main/webapp/video-poc/js/controller/step3Controller.js
@@ -97,6 +97,26 @@ myAppModule.controller('step3Controller', function($scope, sharedService, $locat
 			alert("The video has not yet started playing. Try logging after you have started playing the video");
 		}
 	}
+	
+	$scope.publishVideoLog = function (type) {
+		var d = new Date();
+		var pid = d.getTime();
+//		console.log(JSON.stringify($scope.selectedVideo));
+		$.ajax({
+			  contentType:"text/plain;charset=UTF-8",
+			  type: "POST",
+			  url: '/video-logger/getVideoLog?publish=true&pid='+pid,
+			  data: JSON.stringify($scope.selectedVideo),
+			  success: function (data) {
+				  console.log(data);
+				  window.open('/video-logger/getVideoLog?publish=true&show=y&pid='+pid,'_blank');
+			  },
+			  error: function (data) {
+//				  console.log(data);
+			  },
+			  dataType: "json"
+		});
+	}
 		
 	// Save the video logs
 	$scope.saveVideoLog = function (type) {
