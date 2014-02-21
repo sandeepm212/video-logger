5116c600df03a6ccc630e54b1af94a98d713523d
sandeep.mandrumaka@gmail.com <sandeep.mandrumaka@gmail.com@1f31e873-1701-5331-db18-291316ffd775>
2014-02-21 17:27:19 +0000

    sorting based on created date

--- trunk/src/main/java/com/cognizant/videologger/web/SaveVideoLog.java
+++ trunk/src/main/java/com/cognizant/videologger/web/SaveVideoLog.java
@@ -6,7 +6,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
 
@@ -53,8 +55,8 @@ public class SaveVideoLog extends HttpServlet {
 				}
 			}
 		}
-		
-		Gson gson = new Gson();		
+
+		Gson gson = new Gson();
 		String newProjId = null;
 		Video videoLog = gson.fromJson(jsonBody.toString(), Video.class);
 		if (videoLog != null) {
@@ -62,17 +64,36 @@ public class SaveVideoLog extends HttpServlet {
 				newProjId = System.currentTimeMillis() + "" + videoLog.getUserId();
 				videoLog.setProjectId(newProjId);
 			}
-			videoLog.setLastSavedDate(new Date().getTime()+"");
-			ApplicationContextListener.VIDEO_LOG_PROJECTS.put(videoLog.getProjectId() + "" + videoLog.getUserId(), videoLog);
+			videoLog.setLastSavedDate(new Date().getTime() + "");
+			ApplicationContextListener.VIDEO_LOG_PROJECTS.put(videoLog.getProjectId() + "" + videoLog.getUserId(),
+					videoLog);
 		}
-		
-		String gsonData = gson.toJson(ApplicationContextListener.VIDEO_LOG_PROJECTS.values());
+
+		List<Video> logs = new ArrayList<Video>(ApplicationContextListener.VIDEO_LOG_PROJECTS.values());
+		Collections.sort(logs, projComparator);
+		String gsonData = gson.toJson(logs);
+
 		Utils.writeToFile(gsonData, new File("videolog.data"));
-		
+
 		resp.setContentType("text/json");
 		PrintWriter out = resp.getWriter();
 		String response = "{\"projectId\":\"" + newProjId + "\"}";
 		System.out.println("response::[" + response + "]");
 		out.println(response);
 	}
+
+	public static Comparator<Video> projComparator = new Comparator<Video>() {
+		public int compare(Video log1, Video log2) {
+
+			Long l1 = Long.parseLong(log1.getProjectId());
+			Long l2 = Long.parseLong(log2.getProjectId());
+
+			// ascending order
+			return l2.compareTo(l1);
+
+			// descending order
+			// return fruitName2.compareTo(fruitName1);
+		}
+
+	};
 }
