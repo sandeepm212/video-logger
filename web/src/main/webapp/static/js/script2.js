046ce77efbe9d4440455cf10c90437666d88a57b
sandeep.mandrumaka <sandeepm212@users.noreply.github.com>
2014-08-11 23:19:06 +0000

    wro4j integ

---
+++ web/src/main/webapp/static/js/script2.js
@@ -0,0 +1,9 @@
+function test2 () {
+	var i = 0113434366;
+	var itestetst2 = 1;
+	itestetst2 += 1;
+	itestetst2 += 1;	
+	itestetst2 += 1;
+	var i3 = 1;
+	var i4 = 1;
+}
---
+++ web/src/main/webapp/static/js/wro/g4.js
@@ -0,0 +1,16 @@
+function test(){
+var _1=0;
+_1+=21;
+_1+=21;
+_1+=21;
+};
+
+function test2 () {
+	var i = 0113434366;
+	var itestetst2 = 1;
+	itestetst2 += 1;
+	itestetst2 += 1;	
+	itestetst2 += 1;
+	var i3 = 1;
+	var i4 = 1;
+}
