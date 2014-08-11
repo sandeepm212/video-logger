dbd6899d4a76d4f07de7683399d634a655cb9364
sandeep.mandrumaka@gmail.com <sandeep.mandrumaka@gmail.com@1f31e873-1701-5331-db18-291316ffd775>
2014-08-11 23:19:06 +0000

    wro4j integ

---
+++ trunk/web/src/main/webapp/static/js/g4.js
@@ -0,0 +1,8 @@
+
+function test(){var i=0;};
+function test2 () {
+	var i = 0113434366;
+	var i2 = 1;
+	var i3 = 1;
+	var i4 = 1;
+};
---
+++ trunk/web/src/main/webapp/static/js/script1.js
@@ -0,0 +1,6 @@
+function test () {
+	var testname = 0;
+	testname += 21;
+	testname += 21;
+	testname += 21;
+}
